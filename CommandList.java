9b6e6b2d34d2dd496e2d16959850694222fd13c2
John Loyd de Troz <john.detroz@obf.ateneo.edu>
2020-07-08 11:39:43 +0800

    Consolidated commands into a single method

--- CommandList.java
+++ CommandList.java
@@ -4,7 +4,7 @@ public class CommandList {
     private static String[] commands = new String[10];
     private static String[] keys = new String[10];
 
-    public static String[] getCommands() {
+    private static void initializeCommands() {
         commands[0] = "get balance";
         commands[1] = "get coupons";
         commands[2] = "get open branches nearby";
@@ -15,6 +15,10 @@ public class CommandList {
         commands[7] = "list branches";
         commands[8] = "get branch address";
         commands[9] = "get opening hours of branch";
+    }
+
+    public static String[] getCommands() {
+        initializeCommands();
 
         return commands;
     }
@@ -36,16 +40,7 @@ public class CommandList {
     
     public static String printCommandResponse(String command) {
         Hashtable<String, String> hashtable = new Hashtable<>();
-        commands[0] = "get balance";
-        commands[1] = "get coupons";
-        commands[2] = "get open branches nearby";
-        commands[3] = "get my rank";
-        commands[4] = "know ongoing promotions";
-        commands[5] = "void transaction";
-        commands[6] = "deactivate user";
-        commands[7] = "list branches";
-        commands[8] = "get branch address";
-        commands[9] = "get opening hours of branch";
+        initializeCommands();
 
         hashtable.put(commands[0], "Alright, kindly enter the following BALANCE<SPACE>phoneNumber<SPACE>pin<SPACE>merchantName. Example: BALANCE 09176780012 123456 Angus");
         hashtable.put(commands[1], "Alright, kindly enter the following COUPONS<SPACE>phoneNumber<SPACE>pin<SPACE>merchantName. Example: COUPONS 09176780012 123456 Angus");
@@ -67,17 +62,7 @@ public class CommandList {
     }
 
     public static int validateKeywords(String command, String[] args) {
-        Hashtable<String, String> hashtable = new Hashtable<>();
-        commands[0] = "get balance";
-        commands[1] = "get coupons";
-        commands[2] = "get open branches nearby";
-        commands[3] = "get my rank";
-        commands[4] = "know ongoing promotions";
-        commands[5] = "void transaction";
-        commands[6] = "deactivate user";
-        commands[7] = "list branches";
-        commands[8] = "get branch address";
-        commands[9] = "get opening hours of branch";
+        initializeCommands();
 
         // command | number of fields/args (Not including keyword)
         Hashtable<String, Integer> table = new Hashtable<>();
