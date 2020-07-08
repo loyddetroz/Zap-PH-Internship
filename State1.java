667c5c5615a12f15d91388d2532e353d5c7d341d
jasonlimcaco <45145983+jasonlimcaco@users.noreply.github.com>
2020-07-08 16:52:18 +0800

    Update State1.java
    trigger to state 3

--- State1.java
+++ State1.java
@@ -10,18 +10,23 @@ public class State1 extends State {
         String list = "List of Request Commands: ";
         for (String str : commands) {
             list = list + "\n" + str;
-        }
-        for (int i = 0; i < keys.length; i++) {
-			if(input.toUpperCase().equals(keys[i])) {
-				input = commands[i];
-			}
-		}
+	}
 
         resultState.setNextMessage(list);
         resultState.setCommand("");
         resultState.setNextState(0);
-
-        if (Arrays.asList(commands).contains(input.toLowerCase())) {
+	    
+	if (Arrays.asList(keys).contains(input.toUpperCase())) {
+        	for (int i = 0; i < keys.length; i++) {
+    			if(input.toUpperCase().equals(keys[i])) {
+    				input = commands[i];
+    			}
+    		}
+        	resultState.setNextState(2);
+            resultState.setCommand(command.toLowerCase());
+            resultState.setNextMessage(CommandList.printCommandResponse(command));
+        }
+        else if (Arrays.asList(commands).contains(input.toLowerCase())) {
             resultState.setNextState(1);
             resultState.setCommand(input.toLowerCase());
             resultState.setNextMessage("I would like to confirm if you wish to " + input.toLowerCase() + ".");
