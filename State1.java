abc458caa5c44fbea280cf01f04ed5cc5950a2c6
jasonlimcaco <45145983+jasonlimcaco@users.noreply.github.com>
2020-07-08 11:21:11 +0800

    Update State1.java

--- State1.java
+++ State1.java
@@ -5,11 +5,17 @@ public class State1 extends State {
     @Override
     public ResultState process(String input, String command) {
         String[] commands = CommandList.getCommands();
+        String[] keys = CommandList.getKeys();
         ResultState resultState = new ResultState();
         String list = "\n" + "List of Request Commands: ";
         for (String str : commands) {
             list = list + "\n" + str;
         }
+        for (int i = 0; i < keys.length; i++) {
+			if(input.toUpperCase().equals(keys[i])) {
+				input = commands[i];
+			}
+		}
         resultState.setNextMessage(list);
         resultState.setCommand("");
         resultState.setNextState(0);
