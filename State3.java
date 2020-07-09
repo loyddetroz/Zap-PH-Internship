43cb8ef826dbceb78905c39f42a333546d54f472
jasonlimcaco <45145983+jasonlimcaco@users.noreply.github.com>
2020-07-09 15:17:27 +0800

    Update State3.java

--- State3.java
+++ State3.java
@@ -7,12 +7,35 @@ public class State3 extends State {
         String[] commands = CommandList.getCommands();
         ResultState resultState = new ResultState();
         String[] entries = input.split(" ");
+        String[] keys = CommandList.getKeys();
         String[] modifiedEntries = Arrays.copyOfRange(entries, 1, entries.length);
         int n = CommandList.validateKeywords(command, modifiedEntries);
-
+        
         // to do
         // validate branches or merchant names with spaces
-        if (Arrays.asList(commands).contains(input)) {
+        if (Arrays.asList(keys).contains(entries[0].toUpperCase())) {
+            for (int i = 0; i < keys.length; i++) {
+                if(entries[0].toUpperCase().equals(keys[i])) {
+                	entries[0] = commands[i];
+                }
+            }
+            int m = CommandList.validateKeywords(entries[0], modifiedEntries);
+            if (modifiedEntries.length == m) {
+                Main.setData(modifiedEntries);
+                resultState.setNextState(3);
+                resultState.setCommand(input);
+                resultState.setNextMessage("Confirming request to " + input + ".");
+            } else if (modifiedEntries.length < m){
+                resultState.setNextState(2);
+                resultState.setCommand(entries[0]);
+                resultState.setNextMessage("You lack a field kindly re-enter the requested information." + input);
+            } else {
+                resultState.setNextState(2);
+                resultState.setCommand(entries[0]);
+                resultState.setNextMessage("Invalid input. Please try again.");
+            }
+        }
+        else if (Arrays.asList(commands).contains(input)) {
             resultState.setNextState(2);
             resultState.setCommand(input.toLowerCase());
             resultState.setNextMessage(CommandList.printCommandResponse(input));
