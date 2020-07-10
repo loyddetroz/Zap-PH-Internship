01b2f55c751e936095f6c24fd228e3a75bd0f164
jasonlimcaco <45145983+jasonlimcaco@users.noreply.github.com>
2020-07-10 11:45:18 +0800

    Update State3.java

--- State3.java
+++ State3.java
@@ -15,22 +15,22 @@ public class State3 extends State {
             resultState.setNextState(2);
             resultState.setCommand(input.toLowerCase());
             resultState.setNextMessage(CommandList.printCommandResponse(input));
-        } else if (Arrays.asList(keys).contains(entries[0]) && modifiedEntries.length == n) {
+        } else if (Arrays.asList(keys).contains(entries[0].toUpperCase()) && modifiedEntries.length == n) {
             for (int i = 0; i < keys.length; i++) {
                 if (entries[0].toUpperCase().equals(keys[i])) {
-                    input = commands[i];
+                    entries[0] = commands[i];
                 }
             }
 
             Main.setData(modifiedEntries);
             resultState.setNextState(3);
-            resultState.setCommand(input);
+            resultState.setCommand(entries[0]);
             resultState.setNextMessage("Confirming request to " + input + ".");
-        } else if (Arrays.asList(keys).contains(entries[0]) && modifiedEntries.length < n){
+        } else if (Arrays.asList(keys).contains(entries[0].toUpperCase()) && modifiedEntries.length < n){
             resultState.setNextState(2);
             resultState.setCommand(command);
             resultState.setNextMessage("You lack a field kindly re-enter the requested information.");
-        } else if (Arrays.asList(keys).contains(entries[0]) && modifiedEntries.length > n) {
+        } else if (Arrays.asList(keys).contains(entries[0].toUpperCase()) && modifiedEntries.length > n) {
             resultState.setNextState(2);
             resultState.setCommand(command);
             resultState.setNextMessage("Invalid input. Please try again.");
