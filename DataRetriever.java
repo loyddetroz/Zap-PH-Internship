3ad58f0e263b8eba19eef9bd59aefcea1bdd31a7
John Loyd de Troz <john.detroz@obf.ateneo.edu>
2020-07-10 13:21:53 +0800

    Add listBranches method

--- DataRetriever.java
+++ DataRetriever.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class DataRetriever {
 
     public static void main(String[] args) {
-        System.out.println(getBalance("639000000010", "100009", "Angus_Steakhouse"));
+//        System.out.println(getBalance("639000000010", "100009", "Angus_Steakhouse"));
+        System.out.println(listBranches("Gleeful Lemon"));
     }
 
 	public static String getBalance(String number, String pin, String merchantName){
@@ -13,24 +14,27 @@ public class DataRetriever {
         merchantName = merchantName.replace("_", " ");
         String merchantCode = merchants.get(merchants.indexOf(merchantName)-1).toString();
 
-        if (users.contains(number) && users.get(users.indexOf(number)+1).toString().equals(pin)) {
-            if (merchants.contains(merchantName)) {
-                if (balance.contains(number) && balance.contains(merchantCode)) {
-                    for (int i = 0; i < balance.size(); i++) {
-                        if (balance.get(i).toString().equals(number) && balance.get(i+1).toString().equals(merchantCode)) {
-                            return balance.get(i+2).toString();
+        if (users.contains(number)) {
+            if (users.get(users.indexOf(number)+1).toString().equals(pin) ){
+                if (merchants.contains(merchantName)) {
+                    if (balance.contains(number) && balance.contains(merchantCode)) {
+                        for (int i = 0; i < balance.size(); i++) {
+                            if (balance.get(i).toString().equals(number) && balance.get(i+1).toString().equals(merchantCode)) {
+                                return balance.get(i+2).toString();
+                            }
                         }
+                    } else {
+                        return "The number or merchant is not in the database.";
                     }
                 } else {
-                    return "The number or merchant is not in the database.";
+                    return "Invalid merchant name.";
                 }
             } else {
-                return "Invalid merchant name.";
+                return "Incorrect pin.";
             }
 
-
         } else {
-            return "Incorrect number or pin.";
+            return "Invalid number.";
         }
 
 		return "Balance not found";
@@ -45,26 +49,73 @@ public class DataRetriever {
     }
 
     public static String getOpenBranches() {
-        return "";
+        return "Functionality not supported as of the moment.";
     }
 
     public static String getRank(String number, String pin, String merchantName) {
         ArrayList users = readFile("data/users");
         ArrayList merchants = readFile("data/merchants");
         ArrayList userRanks = readFile("data/user_ranks");
-        ArrayList ranks = readFile("data/ranks")
-        String id = ranks.ge
+        ArrayList ranks = readFile("data/ranks");
 
-        if (users.contains(number) && users.get(users.indexOf(number)+1).toString().equals(pin)) {
-            if (merchants.contains(merchantName)) {
+        if (users.contains(number)) {
+            if (users.get(users.indexOf(number)+1).toString().equals(pin) ){
+                if (merchants.contains(merchantName)) {
 
+                } else {
+                    return "Invalid merchant name.";
+                }
             } else {
-                return "Invalid merchant name.";
+                return "Incorrect pin.";
             }
         } else {
-            return "Incorrect number or pin.";
+            return "Invalid number.";
         }
-        return "";
+
+        return "Rank not available";
+    }
+
+    public static String getPromotions() {
+        return "Functionality not supported as of the moment.";
+    }
+
+    public static String voidTX() {
+        return "Functionality not supported as of the moment.";
+    }
+
+    public static String deactivate() {
+        return "Functionality not supported as of the moment.";
+    }
+
+    public static String listBranches(String merchantName) {
+        ArrayList branches = readFile("data/branches");
+        ArrayList merchants = readFile("data/merchants");
+        merchantName = merchantName.replace("_", "");
+        String merchantCode = "";
+        String branchList = "";
+
+        if (merchants.contains(merchantName)) {
+            merchantCode = merchants.get(merchants.indexOf(merchantName)-1).toString();
+        } else {
+            return "Invalid merchant name.";
+        }
+
+        if (branches.contains(merchantCode)) {
+            for (int i = 0; i < branches.size(); i++) {
+                if (branches.get(i).toString().contains(merchantName)) {
+                    branchList = branchList + branches.get(i).toString() + "\n";
+                }
+            }
+        } else {
+            return "Invalid merchant name.";
+        }
+
+        if (branchList.equals("")) {
+            return "No branch was found";
+        } else {
+            return branchList;
+        }
+
     }
 
 	public static ArrayList<String> readFile(String path) {
